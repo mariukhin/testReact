24875f6fa5c494c32cd6799d512a4e95a9097d19
mariukhin <maryuhin.z@gmail.com>
2019-08-17 20:26:14 +0300

    fixing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { ToastProvider } from 'react-toast-notifications';
 import StartPage from '../pages/StartPage/StartPage';
 import CartPage from '../pages/CartPage/CartPage';
